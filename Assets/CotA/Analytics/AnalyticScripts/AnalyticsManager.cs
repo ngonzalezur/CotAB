255d8050110283c524138bf3ad2d567eef669477
DESKTOP-TTQ87GL\NICOLAS <nicolas.0914.lg@gmail.com>
2025-04-08 22:12:03 -0500

    Sigo tratando de que los datos sean validos en cloud(hago este comit para aceptar un pr)

--- Assets/CotA/Analytics/AnalyticScripts/AnalyticsManager.cs
+++ Assets/CotA/Analytics/AnalyticScripts/AnalyticsManager.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class AnalyticsManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Awake()
+    private async void Awake()
     {
-        UnityServices.InitializeAsync();
+        Debug.Log("Awake ha sido llamado");
+        await UnityServices.InitializeAsync();
         AnalyticsService.Instance.StartDataCollection();
+        Debug.Log("Unity Services inicializados correctamente");
+    }
+    public void SendCustomEvent()
+    {
+        CustomEvent myEvent = new CustomEvent("TestEvent2")
+        {
+            { "testParameter2", 14 }
+        };
+
+        AnalyticsService.Instance.RecordEvent(myEvent);
+        Debug.Log("Evento personalizado enviado correctamente");
     }
 
     void Start()
     {
-        
+        SendCustomEvent();
+        Debug.Log("See llama la funcion");
     }
 
     // Update is called once per frame
