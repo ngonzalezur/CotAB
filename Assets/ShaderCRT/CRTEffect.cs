a1a1370e0ef0b894d2151ac6857ca97a6ea7ba3a
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2025-05-18 19:49:50 -0500

    Shader CRT implementado

---
+++ Assets/ShaderCRT/CRTEffect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CRTEffect : MonoBehaviour
+{
+    [SerializeField] private Material crtMaterial;
+
+    [Range(0, 1)]
+    public float scanlineIntensity = 0.3f;
+    [Range(0, 1000)]
+    public float scanlineFrequency = 480.0f;
+    [Range(0, 0.1f)]
+    public float distortionAmount = 0.03f;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (crtMaterial != null)
+        {
+            // Actualizar los parámetros del material
+            crtMaterial.SetFloat("_ScanlineIntensity", scanlineIntensity);
+            crtMaterial.SetFloat("_ScanlineFrequency", scanlineFrequency);
+            crtMaterial.SetFloat("_DistortionAmount", distortionAmount);
+
+            // Aplicar el efecto
+            Graphics.Blit(source, destination, crtMaterial);
+        }
+        else
+        {
+            // Si no hay material, simplemente copia la textura
+            Graphics.Blit(source, destination);
+        }
+    }
+}
+
+
+
+
+
