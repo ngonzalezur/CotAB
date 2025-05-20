732945404dc8a35f941da760afeab32003d9f62e
GERTRUDIS_DAVID\david <david.garcia.cespedes@gmail.com>
2025-05-20 16:51:28 -0500

    Creación de la primera escena para cinematicas usando signal emitters para cambiar entre escenas, funciona melo

--- Assets/Scenes Cota/ChangeScene.cs
+++ Assets/Scenes Cota/ChangeScene.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class ChangeScene : MonoBehaviour
 {
+    public PlayableDirector timeline; // Para usar el timeline para cambiar escenas
     public void Tutorial()
     {
         SceneManager.LoadScene("Tutorial");
@@ -12,6 +14,7 @@ public class ChangeScene : MonoBehaviour
     public void LevelEasy()
     {
         SceneManager.LoadScene("Level easy");
+        Debug.Log("Puto");
     }
     public void LevelMedium()
     {
