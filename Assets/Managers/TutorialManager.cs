bb48b89f89a495b15d557977b163fa5a05a1395b
DESKTOP-TTQ87GL\NICOLAS <nicolas.0914.lg@gmail.com>
2025-05-22 11:17:10 -0500

    Seteo de escena para tutorial y pseudocodigo para la imprementacion

---
+++ Assets/Managers/TutorialManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    // Mostrar teclas de movimiento
+    // Highlight casilla aleatoria
+    // Empezar timer
+    // Acceder a la casilla del jugador
+    // Si casilla highlight == casilla jugador: contador +1
+    // Si contador == 3, siguiente tutorial
+    // Si timer > 30s: play sonido explicativo
+
+    // Mostrar teclas de movimiento
+    // Highlight casilla aleatoria
+    // Empezar timer
+    // Acceder a la casilla del jugador
+    // Si casilla highlight == casilla jugador contador: Higlight casilla aleatoria
+    // Si Estamina == 0: Tachas teclas de movimiento y mostrar barra vacia
+    // Si timer > 30s: play sonido explicativo
+
+    // Mostarr UI hechizos
+    // Mostrar boton presionado 
+    // Si casilla del dummy == casilla hechizo: Contador2 +1
+    // Si contador2 == 2: siguiente tutorial
+    // Si casilla del dummy != casilla hechizo: PLay sonido explicativo
+    // Si Presiona tecla hechizo en CD: Play sonido explicativo y mostrar UI tachada
+
+    // Dummy lanza proyectiles
+    // Proyectil desaparece: Contador +1
+    // Contador == 3: fin de tutorial
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
