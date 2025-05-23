1d41198c34808f64f3e0da6089f4f4000732a57b
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2025-05-22 19:38:56 -0500

    modificación del IA

--- Assets/Behaviours/EnemyFollowsHero.cs
+++ Assets/Behaviours/EnemyFollowsHero.cs
@@ -14,8 +14,12 @@ public partial class EnemyFollowsHero : Action
     [SerializeReference] public BlackboardVariable<BaseUnit> Hero;
     [SerializeReference] public BlackboardVariable<int> Column;
 
+
+
     protected override Status OnStart()
     {
+
+
         BaseUnit persistentHero = GameObject.FindObjectsByType<BaseUnit>(FindObjectsSortMode.None)
                                      .FirstOrDefault(u => u.isPersistentHero);
         if (persistentHero == null)
@@ -31,8 +35,12 @@ public partial class EnemyFollowsHero : Action
 
     protected override Status OnUpdate()
     {
+
+
         if (Enemy.Value.OccupiedTile == null || Hero.Value.OccupiedTile == null)
             return Status.Failure;
+
+
 
         int enemyX = Enemy.Value.OccupiedTile.PositionXTile();
         int enemyY = Enemy.Value.OccupiedTile.PositionYTile();
@@ -42,24 +50,28 @@ public partial class EnemyFollowsHero : Action
         if (enemyX < Column)
         {
             Enemy.Value.MoveRight();
-            return Status.Running;
+
+           // return Status.Running;
         }
         else if (enemyX > Column)
         {
             Enemy.Value.MoveLeft();
-            return Status.Running;
+
+           // return Status.Running;
         }
 
         // Ya está en la columna correcta, entonces seguir la fila del héroe
         if (enemyY < heroY)
         {
             Enemy.Value.MoveUp();
-            return Status.Running;
+
+          //  return Status.Running;
         }
         else if (enemyY > heroY)
         {
             Enemy.Value.MoveDown();
-            return Status.Running;
+
+            //return Status.Running;
         }
 
         return Status.Success;
