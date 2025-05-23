48f4501160b4cdb6b5d06fe069d1c2a40cb034aa
GERTRUDIS_DAVID\david <david.garcia.cespedes@gmail.com>
2025-05-22 22:35:44 -0500

    Nuevos SFX para todos, ahora estan hechos para que ayuden a distinguir de donde viene el daño o el ataque, los enemigos ya tienen sfx de daño y ataque, la fachada de sound manager quedo lista

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,13 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID CHANGECOMBATMUSIC = 3740741074U;
+        static const AkUniqueID PLAY_AREA_DISTANCE_ENEMY = 45490180U;
+        static const AkUniqueID PLAY_AREA_FRONT_ENEMY = 553071288U;
+        static const AkUniqueID PLAY_ATRACT_ENEMY = 3136587618U;
+        static const AkUniqueID PLAY_BARRIDO_COLUMNA_BOSS = 1359721473U;
+        static const AkUniqueID PLAY_BARRIDO_FILA_BOSS = 342642858U;
         static const AkUniqueID PLAY_BROTE_ROBOT = 3671704113U;
+        static const AkUniqueID PLAY_CASILLA_X_BOSS = 2362712266U;
         static const AkUniqueID PLAY_CINE01CABALLERO = 865224105U;
         static const AkUniqueID PLAY_CINE01DRUID = 1769257584U;
         static const AkUniqueID PLAY_CINE02CABALLERO = 749673500U;
@@ -24,10 +30,18 @@ namespace AK
         static const AkUniqueID PLAY_CINE04CABALLERO = 3696747394U;
         static const AkUniqueID PLAY_CINE04DRUID = 3372602331U;
         static const AkUniqueID PLAY_COMBATMUSICPLAYLIST = 1462092269U;
+        static const AkUniqueID PLAY_DAMAGE_BOSS = 241575375U;
         static const AkUniqueID PLAY_DAMAGE_DRUID = 3230461460U;
+        static const AkUniqueID PLAY_DAMAGE_HONGO = 245375377U;
+        static const AkUniqueID PLAY_DAMAGE_MURCIELAGO = 3690804762U;
         static const AkUniqueID PLAY_DAMAGE_ROBOT = 1942325012U;
+        static const AkUniqueID PLAY_DAMAGE_SKELETON = 966081817U;
         static const AkUniqueID PLAY_FIREWALL_DRUID = 364273755U;
+        static const AkUniqueID PLAY_FLOR_ATTACK_ROBOT = 2956068371U;
+        static const AkUniqueID PLAY_GANCHO_DRUID = 1159642833U;
+        static const AkUniqueID PLAY_GANCHO_ENEMY = 2573167197U;
         static const AkUniqueID PLAY_GRID_DRUID = 104012431U;
+        static const AkUniqueID PLAY_GRID_TELEPORT_ENEMY = 1041240749U;
         static const AkUniqueID PLAY_MELEE_DRUID = 2651910159U;
         static const AkUniqueID PLAY_MUSICCINEPLAYLIST = 917750874U;
         static const AkUniqueID PLAY_PARRY_ROBOT = 3072108541U;
