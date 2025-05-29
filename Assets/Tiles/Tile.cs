cb47702324be1cc9b0f43e4d1675476eb0824013
Mauricio <mcastanou@eafit.edu.co>
2025-05-29 14:40:36 -0500

    va, se supone pues que eso esta montadito, cambie la forma de recolectar algunos d elos datos

--- Assets/Tiles/Tile.cs
+++ Assets/Tiles/Tile.cs
@@ -59,6 +59,13 @@ public abstract class Tile : MonoBehaviour
     {
         if (unit == null || unit.Faction != Faction || this.OccupiedUnit != null) return;
         // Asegurar que la casilla anterior detenga su animación
+        int pj = 0;
+        if(unit.UnitName == "Druid")
+        {
+            pj = 1;
+        }
+        //string msj = "" + pj + "," + x + "," + y + "," + Time.time;
+        AnalyticsManager.Instance.SendCustomEventMovement("" + pj + "," + x + "," + y + "," + Time.time);
         if (unit.OccupiedTile != null)
         {
             unit.OccupiedTile.StopMovingCoroutine();
